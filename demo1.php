2f4e0d3d0738076274657b276a97720c092f9e13
catiscat <JohnWillimsunnyday@tutanota.com>
2016-01-23 23:02:01 +0800

    mysqlDemo1

--- demo1.php
+++ demo1.php
@@ -21,7 +21,11 @@
  
  //5 接受返回的结果，并处理。
  //mysql_fecth_row 会一次取出$res结果集的下一行数据，并依次赋值给$row（是一个数组）;
+ 
+ echo 'hello0';
+ 
   while($row=mysql_fetch_row($res)){
+   echo 'hellowhile';
   //第一种取法
   //  echo "</br> $row[0]--$row[1]--$row[2]";
   //第二种取法
@@ -31,10 +35,14 @@
     echo "<br/>";
   }
   
+  echo 'hello1';
+  mysql_error();
+  echo 'hello2';
   //关闭数据库连接，释放资源
  mysql_free_result($res);
   //mysql_close($conn)可以没有（因为即使没有，系统也会自动关闭），但是建议有。
   //mysql_close($conn);写在while函数前边也是可以的。因为mysql执行完指令后，包含的那些资源都存在内存里了，跟连接就没有太大关系了。但如果把mysql_free_result($res)写在while函数前就不行，因为这个函数把内存给释放了。
  mysql_close($conn);
-mysql_error();
+ 
+
 ?>
