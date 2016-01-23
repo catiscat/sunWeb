0b0427a1e188cfe551e1fcf1b32c4fe7ff9d94b3
catiscat <JohnWillimsunnyday@tutanota.com>
2016-01-23 22:19:43 +0800

    mysqlDemo1

--- demo1.php
+++ demo1.php
@@ -13,6 +13,25 @@
   //4 发送sql指令
   
   $sql="select * from user1";
+  //$res 表示结果集，可以理解为就是一张表
   $res=mysql_query($sql,$conn);
-  var_dump($res);
+ // var_dump($res);
+ 
+ //5 接受返回的结果，并处理。
+ //mysql_fecth_row 会一次取出$res结果集的下一行数据，并依次赋值给$row（是一个数组）;
+  while($row=mysql_fetch_row($res)){
+  //第一种取法
+  //  echo "</br> $row[0]--$row[1]--$row[2]";
+  //第二种取法
+    foreach($row as $key=>$val){
+      echo "--$val";
+    }
+    echo "<br/>"
+  }
+  
+  //关闭数据库连接，释放资源
+  mysql_free_result($res);
+  //mysql_close($conn)可以没有，但是建议有。
+  mysql_close($conn);
+  
 ?>
