ee5bbebdc4f3a5abf51efc0a300d4dead6a1bb98
catiscat <JohnWillimsunnyday@tutanota.com>
2016-01-25 20:02:04 +0800

    word online

---
+++ wordline/wordProcess.php
@@ -0,0 +1,28 @@
+<?php 
+
+  require_once 'SqlTool.class.php'
+  //接受英文单词
+  if(isset($_POST['enword'])){
+    $en_word=$_POST['enword'];
+  }else{
+    echo "输入为空";
+    echo "<a href='mainView.php'>返回重新查询</a>";
+  }
+  
+  //看看数据库中有没有这条记录, 加上 limit 0,1 效率会更高，指的是找到一个就返回。下面的不再去执行。
+  //原则：用什么查什么
+  $sql="select chword from words where enword=$en_word limit 0,1";
+  //设计表
+  //查询
+  $sqlTool=new SqlTool();
+  $res=$sqlTool->execute_dql($sql);
+  if($row=mysql_fetch_assoc($res)){
+    echo $en_word."对应的中文意思是".$row['chword'];  
+
+  }else{
+    echo "c查询没有这个词条";
+    echo "<br/><a href='mainView.php'>返回重新查询</a>";
+  }
+  
+  mysql_free_result($res);
+?>
