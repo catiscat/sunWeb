2b591072d45e2a98b798bfe73db4d170b631bf4b
catiscat <JohnWillimsunnyday@tutanota.com>
2016-01-25 20:24:46 +0800

    wordline

--- wordline/mainView.php
+++ wordline/mainView.php
@@ -5,7 +5,7 @@
   </head>
     <img src="word.jpe" />
     <h1>查询英文</h1>
-  <form action='??' method="post" >
+  <form action='wordProcess.php' method="post" >
     请输入英文：<input type='text' name="enword" />
     <input type="hidden" value="search" name="type" />
     <inout type="submit" value="查询"/>
