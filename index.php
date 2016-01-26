ba7e4c217aa3d24f86bed08982d59807e8a1f0ae
catiscat <JohnWillimsunnyday@tutanota.com>
2016-01-27 00:26:28 +0800

    add navbar and footer

--- index.php
+++ index.php
@@ -8,18 +8,21 @@
     <title>DuckBlog</title>
   </head>
   <body>
-	<ul>
-	  <li><a class="active" href="#home">Home</a></li>
-	  <li><a href="#Blog">Blog</a></li>
-	  <li><a href="#contact">Contact</a></li>
-	  <li><a href="#about">About</a></li>
-	</ul>
-
+	<nav>
+		<ul>
+		  <li><a class="active" href="#home">Home</a></li>
+		  <li><a href="#Blog">Blog</a></li>
+		  <li><a href="#contact">Contact</a></li>
+		  <li><a href="#about">About</a></li>
+		</ul>
+	</nav>
 	<div class="div0">
 
 
 
 	</div>
-
+    <footer class="footer1">
+        <a href="www.dogduck.lol">Made By Duck</a>
+    </footer>
   </body>
 </html>
