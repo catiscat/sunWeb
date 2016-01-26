b60db820fecf7385650371d7ec50a96c7e50f528
catiscat <JohnWillimsunnyday@tutanota.com>
2016-01-27 00:05:20 +0800

    index.php

--- index.php
+++ index.php
@@ -4,31 +4,22 @@
     <meta charset="utf-8">
     <link rel="shortcut icon" href="images/duckdog.ico" >
     <link rel="stylesheet" href="styles/index.css" type="text/css"  >
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
     <title>DuckBlog</title>
   </head>
   <body>
-	   <nav class="navbar navbar-inverse">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="#">WebSiteName</a>
-        </div>
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Home</a></li>
-          <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1
-            <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Page 1-1</a></li>
-              <li><a href="#">Page 1-2</a></li>
-              <li><a href="#">Page 1-3</a></li>
-            </ul>
-          </li>
-          <li><a href="#">Page 2</a></li>
-          <li><a href="#">Page 3</a></li>
-        </ul>
-      </div>
-  </nav>
-    <h1>welcome to DuckBlog</h1>
+	<ul>
+	  <li><a class="active" href="#home">Home</a></li>
+	  <li><a href="#Blog">Blog</a></li>
+	  <li><a href="#contact">Contact</a></li>
+	  <li><a href="#about">About</a></li>
+	</ul>
+
+	<div class="div0">
+
+
+
+	</div>
+
   </body>
 </html>
