86784d5e652ff27761c3b990d3a980fcc2310659
catiscat <JohnWillimsunnyday@tutanota.com>
2016-01-27 02:32:34 +0800

    miner duck icon

--- base.php
+++ base.php
@@ -11,7 +11,8 @@
 	<nav>
 		<ul>
 		<?php
-		  echo "<li><a class='active' href='index.php'><img class='duckIcon' src='images/duck.ico'/>Home</a></li>";
+		  echo "<img class='duckIcon' src='images/duck.ico'/>";
+		  echo "<li><a class='active' href='index.php'>Home</a></li>";
 		  echo "<li><a href='blog.php'>Blog</a></li>";
 		  echo "<li><a href='contact.php'>Contact</a></li>";
 		  echo "<li><a href='about.php'>About</a></li>";
