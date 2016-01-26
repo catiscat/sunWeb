c0ac7033d4908e5f375be1ab72aa7ecd42490bad
catiscat <JohnWillimsunnyday@tutanota.com>
2016-01-27 01:02:18 +0800

    make index.php a template

--- index.php
+++ index.php
@@ -10,10 +10,12 @@
   <body>
 	<nav>
 		<ul>
-		  <li><a class="active" href="#home">Home</a></li>
-		  <li><a href="#Blog">Blog</a></li>
-		  <li><a href="#contact">Contact</a></li>
-		  <li><a href="#about">About</a></li>
+		<?php
+		  echo "<li><a class='active' href='#home'>Home</a></li>";
+		  echo "<li><a href='blog.php'>Blog</a></li>";
+		  echo "<li><a href='#contact'>Contact</a></li>";
+		  echo "<li><a href='#about'>About</a></li>";
+		  ?>
 		</ul>
 	</nav>
 	<div class="div0">
@@ -23,12 +25,10 @@
 	</div>
 	
        <footer>
-
-            <div>
-              <i>DogDuck.lol</i> &copy; 2016 - <?php echo date('Y'); ?>
+            
+              <i>DogDuck.lol 版权所有</i> &copy; 2016 - <?php echo date('Y'); ?>
                <p>转载请注明出处</p>
-            </div>
-
+            
         </footer>
   </body>
 </html>
