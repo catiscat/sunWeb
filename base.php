881d0fdc86a2e05a7ac3c8a7dc7a875086c7e7da
catiscat <JohnWillimsunnyday@tutanota.com>
2016-01-27 01:07:34 +0800

    make base.php a template add blog.php contact.php about.php

---
+++ base.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="images/duckdog.ico" >
+    <link rel="stylesheet" href="styles/index.css" type="text/css"  >
+
+    <title>DuckBlog</title>
+  </head>
+  <body>
+	<nav>
+		<ul>
+		<?php
+		  echo "<li><a class='active' href='index.php'>Home</a></li>";
+		  echo "<li><a href='blog.php'>Blog</a></li>";
+		  echo "<li><a href='contact.php'>Contact</a></li>";
+		  echo "<li><a href='about.php'>About</a></li>";
+		  ?>
+		</ul>
+	</nav>
+	<div class="div0">
+
+
+
+	</div>
+	
+       <footer>
+            
+              <i>DogDuck.lol 版权所有</i> &copy; 2016 - <?php echo date('Y'); ?>
+               <p>转载请注明出处</p>
+            
+        </footer>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,34 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="shortcut icon" href="images/duckdog.ico" >
-    <link rel="stylesheet" href="styles/index.css" type="text/css"  >
-
-    <title>DuckBlog</title>
-  </head>
-  <body>
-	<nav>
-		<ul>
-		<?php
-		  echo "<li><a class='active' href='#home'>Home</a></li>";
-		  echo "<li><a href='blog.php'>Blog</a></li>";
-		  echo "<li><a href='#contact'>Contact</a></li>";
-		  echo "<li><a href='#about'>About</a></li>";
-		  ?>
-		</ul>
-	</nav>
-	<div class="div0">
-
-
-
-	</div>
+<?php 
+    require_once "base.php";
 	
-       <footer>
-            
-              <i>DogDuck.lol 版权所有</i> &copy; 2016 - <?php echo date('Y'); ?>
-               <p>转载请注明出处</p>
-            
-        </footer>
-  </body>
-</html>
+?>
