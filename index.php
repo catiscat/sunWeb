57680d4fcf85a0c004b3bc1e7f92b592c870c38f
catiscat <JohnWillimsunnyday@tutanota.com>
2016-01-26 19:09:47 +0800

    duckblog

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link rel="shortcut icon" href="images/duckdog.ico">
-    <link style="stylesheet" type="text/css" href="css/index.css">
+    <link rel='stylesheet' href='css/index.css' type='text/css'  />
     <title>DuckBlog</title>
   </head>
   <body>
