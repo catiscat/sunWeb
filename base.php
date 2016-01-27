121e05f8d941b659d2647fc493b8d963c550e8d5
catiscat <JohnWillimsunnyday@tutanota.com>
2016-01-27 22:10:14 +0800

    base.css change name

--- base.php
+++ base.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link rel="shortcut icon" href="images/duck.ico" >
-    <link rel="stylesheet" href="styles/index.css" type="text/css"  >
+    <link rel="stylesheet" href="styles/base.css" type="text/css"  >
 
     <title>DuckBlog</title>
   </head>
