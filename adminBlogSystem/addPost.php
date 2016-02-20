006071973d575146df3742eaaaae4cb3e7cb51a5
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-20 13:50:08 +0800

    datetime

--- adminBlogSystem/addPost.php
+++ adminBlogSystem/addPost.php
@@ -3,7 +3,7 @@
     <head>
         <meta http-equiv="content-type" content="text/html;charset=utf-8" />
         <script type="text/javascript">
-            window.onload= function showTime(){               
+            function showTime(){               
                 document.getElementByName("post_date").value = new Date().toLocaleString();
             }
         </script>
@@ -12,10 +12,10 @@
     <form action="postProcess.php" method="post">
         <table>
             <tr><td>post_author</td><td><input type="text" name="post_author" /></td></tr>
-            <tr><td>post_date</td><td><input type="text" name="post_date" value='0' /></td></tr>
+            <tr><td>post_date</td><td><input type="text" name="post_date" value='0' onclick="showTime()" /></td></tr>
 	        <tr><td>post_title</td><td><input type="text" name="post_title" /></td></tr> 
             <tr><td>post_type</td><td><input type="text" name="post_type" /></td></tr> 
-            <tr><td>post_content</td><td><textarea rows="60000" cols="500" type="text" name="post_content" ></textarea></td></tr>
+            <tr><td>post_content</td><td><textarea rows="600" cols="200" type="text" name="post_content" ></textarea></td></tr>
 
 
             <input type="hidden" name="flag" value="addpost" />
