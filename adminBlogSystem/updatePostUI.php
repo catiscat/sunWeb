f6230951887a02c080438f65676e5e38efbc8cd0
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-20 18:31:44 +0800

    readPost.php

--- adminBlogSystem/updatePostUI.php
+++ adminBlogSystem/updatePostUI.php
@@ -2,6 +2,11 @@
 
     <head>
         <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+        <script type="text/javascript">
+            function showTime(){               
+                document.getElementById("post_date").value = new Date().toLocaleString();
+            }
+        </script>
     </head>
     <?php 
 
@@ -23,7 +28,7 @@
         <table>
             <tr><td>ID</td><td><input readonly="readonly" type="text" name="id" value="<?php echo $arr[0]['id'] ?>" /></td></tr>        
             <tr><td>post_author</td><td><input type="text" name="post_author" value="<?php echo $arr[0]['post_author'] ?>" /></td></tr>
-            <tr><td>post_date</td><td><input type="text" name="post_date" value="<?php echo $arr[0]['post_date'] ?>" /></td></tr>
+            <tr><td>post_date</td><td><input type="text" onclick="showTime(this)" name="post_date" value="<?php echo $arr[0]['post_date'] ?>" /></td></tr>
 		    <tr><td>post_title</td><td><input type="text" name="post_title" value="<?php echo $arr[0]['post_title'] ?>" /></td></tr>
             <tr><td>post_type</td><td><input type="text" name="post_type" value="<?php echo $arr[0]['post_type'] ?>" /></td></tr>
             <tr><td>post_content</td><td><textarea type="text" rows="50" cols="100" name="post_content" value="<?php echo $arr[0]['post_content'] ?>" ></textarea></td></tr>         
