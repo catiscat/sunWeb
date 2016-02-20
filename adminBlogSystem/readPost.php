9d7c182d403c3ff567e2ba147776f62298e9532a
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-20 15:47:22 +0800

    specify readPost.php

--- adminBlogSystem/readPost.php
+++ adminBlogSystem/readPost.php
@@ -6,9 +6,20 @@
 
 <?php 
 
+    //该页面要显示指定博文的详细信息
+    require_once 'PostService.class.php';
+    
     $id=$_GET['id'];
     echo "你正在阅读id=".$id."的博文";
-
+    
+    //查询数据库，调用sqlHelper  
+    $postService=new PostService();
+    $arr=$postService->getPostById($id);
 ?>
+    <body>
+        <h3><?php echo $arr[0]['post_title'] ?></h3><br/>
+        <h4><?php echo $arr[0]['post_author'] ?></h4>
+        <pre><?php echo $arr[0]['post_content'] ?></pre>
+    </body>
 
 </html>
