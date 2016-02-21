42d705b1ee02b616bbcfb58696edafbbcf691917
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-21 11:14:03 +0800

    change sqlHelper gotourl

--- about.php
+++ about.php
@@ -27,13 +27,13 @@
                 <p><b>【站点走向】：</b><br><br>该站点建立的初衷是记录我学习工作的点滴并与各位分享。也非常欢迎各位您的分享。</p><br>
                 <p><b>【站长想说】：</b><br><br>现在是2016/2/21/00:59，当回顾该站点的建立过程时，突然发现，这也是自己的一段成长历程。</p>
                 <p>三个月前，我还徘徊在大学生活的迷茫中。还是一个从未接触过代码的小小白。</p>
-                <p>然后某天，在<a href="blog.dogduck.co">dog</a>的强烈建议下，我开始尝试去了解，什么是编程，什么是代码。</p>
+                <p>然后某天，在<a href="https://blog.dogduck.co/">dog</a>的强烈建议下，我开始尝试去了解，什么是编程，什么是代码。</p>
                 <p>接下来是，我跟着廖雪峰的博客学习了【python】基础，看着《Deepin Python3》和《Invent Your Own Computer Games with Python, 2nd Edition (Version 13) (2010)》练了一些小游戏。之所以从【python】开始，是因为她简单易学。</p>
                 <p>后来某天，听闻【Django】是一个基于【python】并用于建站的框架，便怦然心动：自己动手建网站，So Cool！于是便开始捣鼓【Django】，梦想着一个网站在自己的指尖诞生。</p>
                 <p>可是发现，建站也需要前端基础。于是就跟着韩顺平的系列视频学习了【html，css，javascript】，还写了一些网页小游戏。然后又回头捣鼓【Django】，但发现自己基础薄弱，有些内容很难理解，加上手头的实验项目要开始实施。于是，【Django】学习暂告一段落。</p>
-                <p>后来的后来，<a href="blog.dogduck.co">dog</a>告诉我，PHP简单易学，也可以建站。于是，我重新回归代码的怀抱。依旧是跟着韩顺平的系列视频学起了【PHP，Mysql】，一边学一边惦念着尚未诞生的网站。</p>
-                <p>终于，2016/1/15，在<a href="blog.dogduck.co">dog</a>的鼓励下，我开始将【用PHP+MySql建站】纳入计划，准确地说，是边学边建。2016/2/21/，<a href="www.dogduck.lol">Duck</a>站点诞生。它也会在您的关注下继续成长。</p>
-                <p>最后，感谢<a href="blog.dogduck.co">dog</a>一直以来的支持，鼓励和陪伴！</p>
+                <p>后来的后来，<a href="https://blog.dogduck.co/">dog</a>告诉我，PHP简单易学，也可以建站。于是，我重新回归代码的怀抱。依旧是跟着韩顺平的系列视频学起了【PHP，Mysql】，一边学一边惦念着尚未诞生的网站。</p>
+                <p>终于，2016/1/15，在<a href="https://blog.dogduck.co/">dog</a>的鼓励下，我开始将【用PHP+MySql建站】纳入计划，准确地说，是边学边建。2016/2/21/，<a href="www.dogduck.lol">Duck</a>站点诞生。它也会在您的关注下继续成长。</p>
+                <p>最后，感谢<a href="https://blog.dogduck.co/">dog</a>一直以来的支持，鼓励和陪伴！</p>
       </div>
         </div>
       <div class="div3">
