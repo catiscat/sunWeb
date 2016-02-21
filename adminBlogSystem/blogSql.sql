69ae6b41ac4f0d66f0086639e901982cf3dd1904
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-21 12:02:11 +0800

    div height 100%

--- adminBlogSystem/blogSql.sql
+++ adminBlogSystem/blogSql.sql
@@ -9,7 +9,7 @@ create table admin(
 
 --posts table for blog post,blog delete,blog select and blog update
 create table posts(
-    id int primary key auto_increment,
+    id bigint primary key auto_increment,
     post_author varchar(64) not null,
     post_date text,
     post_content longtext not null,
@@ -26,3 +26,19 @@ create table posts(
 
 insert into admin values (901,"c@t*9q",md5("c@t*9q"));
 
+
+--comment table for comment post,comment delete,comment select and comment update
+create table comment(
+    comment_id bigint primary key auto_increment,
+    comment_post_id bigint,
+    comment_author tinytext,
+    comment_date text,
+    comment_content text,
+    comment_approved varchar(20),
+    user_id bigint,
+    comment_type nvarchar(20),
+    comment_parent bigint,
+    comment_author_email varchar(100),
+    comment_author_url varchar(200),
+    comment_author_ip varchar(100)    
+);
