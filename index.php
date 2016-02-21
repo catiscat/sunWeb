ac5b9cd7d459a357c374114acc885ed12dee6df1
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-21 12:25:51 +0800

    div height 90%

--- index.php
+++ index.php
@@ -101,15 +101,16 @@
 
 
 	</div>
-	<div class='goTopDiv'>
-		<a href='index.php#'><img class='goTop' src='images/goTop.ico' /></a>
-	</div>
+
 	<div class="footerDiv">
        <footer>
             
               <i>DogDuck.lol 版权所有</i> &copy; 2016 - <?php echo date('Y'); ?>
                <p>转载请注明出处</p>            
         </footer>
+		<div class='goTopDiv'>
+		<a href='index.php#'><img class='goTop' src='images/goTop.ico' /></a>
+		</div>
 	</div>
   </body>
 </html>
