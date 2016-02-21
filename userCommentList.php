c8940421031b1459721e7101cf6139791797ac32
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-21 16:37:20 +0800

    user_comment_list

--- userCommentList.php
+++ userCommentList.php
@@ -18,7 +18,10 @@
     <?php 
 
 	require_once "adminBlogSystem/comment/CommentService.class.php";
+
+	
 	require_once "adminBlogSystem/comment/RollPage.class.php";
+	
 
 	//创建了一个CommentService的对象实例
 	$commentService=new CommentService();
@@ -54,11 +57,9 @@
         //调用getCommentListByPage方法，获取应当显示的评论信息列表
         $res2=$commentService->getCommentListByPage($rollPage->pageNow,$rollPage->pageSize);
         echo "<table border='1px' width='700px' bordercolor='green' cellspacing='0px'>";
-        echo "<tr><th>comment_id</th><th>comment_author</th><th>comment_date</th><th>comment_content</th><th>comment_post_id</th><th>comment_type</th><th>删除评论</th><th>修改评论</th><th>阅读评论</th></tr>";
-
         for($i=0;$i<count($res2);$i++){
             $row=$res2[$i];
-            echo "<tr><td>{$row['comment_id']}</td><td>{$row['comment_author']}</td><td>{$row['comment_date']}</td><td>{$row['comment_content']}</td><td>{$row['comment_post_id']}</td><td>{$row['comment_type']}</td><td><a onclick='return confirmDel({$row['comment_id']})' href='commentProcess.php?flag=del&id={$row['comment_id']}'>删除评论</td><td><a href='updateCommentUI.php?id={$row['comment_id']}'>修改评论</td><td><a href='readComment.php?id={$row['comment_id']}'>阅读评论</td></tr>";
+            echo "<tr><td>{$row['comment_author']}</td><td>{$row['comment_date']}</td><td>{$row['comment_content']}</td></tr>";
         }
         
         
