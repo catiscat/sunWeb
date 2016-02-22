3ea3a15213011a7c39a6351772ace0319427eceb
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-22 15:28:19 +0800

    sql

--- userCommentProcess.php
+++ userCommentProcess.php
@@ -4,7 +4,7 @@
     require_once "./adminBlogSystem/SqlHelper.class.php";
 	//创建了一个CommentService的对象实例
 	$commentService=new CommentService();
-
+	$sqlHelper=new SqlHelper();
    	//先看看用户要分页还是删除某个评论
 	if(!empty($_REQUEST['flag'])){ 
 	    //接收flag的值
@@ -13,10 +13,10 @@
         if($flag=="addcomment"){
 		    //说明用户要执行添加评论的请求
 		    //接受数据,并将用户输入的html标签都去掉
-		    $comment_author=mysqli_real_escape_string($this->conn,strip_tags($_POST['comment_author']));
-		    $comment_date=mysqli_real_escape_string($this->conn,strip_tags($_POST['comment_date']));
-		    $comment_content=mysqli_real_escape_string($this->conn,strip_tags($_POST['comment_content']));
-		    $comment_post_id=mysqli_real_escape_string($this->conn,strip_tags($_POST['comment_post_id']));
+		    $comment_author=mysqli_real_escape_string($sqlHelper->conn,strip_tags($_POST['comment_author']));
+		    $comment_date=mysqli_real_escape_string($sqlHelper->conn,strip_tags($_POST['comment_date']));
+		    $comment_content=mysqli_real_escape_string($sqlHelper->conn,strip_tags($_POST['comment_content']));
+		    $comment_post_id=mysqli_real_escape_string($sqlHelper->conn,strip_tags($_POST['comment_post_id']));
 		  
 		    
 		    //完成添加-》数据库
