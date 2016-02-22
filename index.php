12de1d06fba5789485e8537f0fb4d36692d38033
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-22 19:55:17 +0800

    sql

--- index.php
+++ index.php
@@ -41,13 +41,25 @@
 	          $rollPage->pageSize=15;
 	          $rollPage->gotoUrl="index.php";
                   
+                  	
+                  //$pageCount=0;//这个表一共有多少页，是计算出来的。
+                 
+                 
+	              //调用了getRollPage($rollPage)方法，该方法可以把分页功能完成
+	              $postService->getRollPage($rollPage);
+	              
+	              
+	              //调用getPageCount方法，获取一共有多少页
+	              $pageCount=$postService->getPageCount($rollPage->pageSize); 
+                  
+                  
                   //这里我们需要根据用户的点击来修改$pageNow的值。
                   //这里我们需要判断 是否有$pageNow 发送，有就使用；如果没有，则默认为显示第一页
                   if(!empty($_GET['pageNow'])){
                       $id=intval(mysql_real_escape_string(strip_tags($_GET['pageNow'])));
                       if(!empty($id)){
-            	        if($id>10000){
-			                header("Location:index.php"); //文章id为空不合法，将用户送回Homepage。
+            	        if($id>$pageCount){
+			                header("Location:index.php"); //文章id大于总页数，将用户送回Homepage。
 		                    exit();
             	         }           
                   }else{
@@ -57,16 +69,7 @@
                       $rollPage->pageNow=$id;  
                   }
                   
-	
-                  //$pageCount=0;//这个表一共有多少页，是计算出来的。
-                 
-                 
-	              //调用了getRollPage($rollPage)方法，该方法可以把分页功能完成
-	              $postService->getRollPage($rollPage);
-	              
-	              
-	              //调用getPageCount方法，获取一共有多少页
-	              $pageCount=$postService->getPageCount($rollPage->pageSize);        
+       
 
 
                   //调用getPostListByPage方法，获取应当显示的博文信息列表
