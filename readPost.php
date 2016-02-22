4ef9f6d5f8e799789a859f46f2e8602ed7e04a6e
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-22 16:38:17 +0800

    sql

--- readPost.php
+++ readPost.php
@@ -36,7 +36,7 @@
             $id=mysql_real_escape_string(strip_tags($_GET['id']));
             
             if(strlen($id)){
-                if(!preg_match("^[0-9]+$",$id) || strlen($id)>5){
+                if(!preg_match("^[0-9]$",$id) || strlen($id)>5){
                     header("Location:index.php"); //文章id不合法，将用户送回Homepage。
 		            exit();
                 }            
