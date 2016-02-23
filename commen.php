f93f6b95215dad3431de6b8f35c19a5154d3ec5a
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-23 19:29:44 +0800

    add session on login.php

--- commen.php
+++ commen.php
@@ -4,11 +4,11 @@
         //首先看看cookie有没有上次登陆信息
         if(!empty($_COOKIE['lastVisit'])){
 
-            echo "朋友，你上次访问<a href=#>duck</a>站点是".$_COOKIE['lastVisit'].",Welcome:)<hr>";
+            echo "<font size='3' color='#737373'>朋友，你上次访问 duck 站点</font>".$_COOKIE['lastVisit']." :)<hr>";
             //更新时间
             setcookie("lastVisit",date("Y-m-d H:i:s"),time()+24*3600*30);
         }else{
-            echo "朋友，你第一次访问<a href=#>duck</a>站点哦,Welcome :)<hr>";
+            echo "朋友，你第一次访问 duck 站点哦 :)<hr>";
             //更新时间
             setcookie("lastVisit",date("Y-m-d H:i:s"),time()+24*3600*30);
         }
