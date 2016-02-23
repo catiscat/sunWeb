21623997c454d517f533d6fa99345b62cbc23003
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-23 20:45:36 +0800

    add session on login.php

--- index.php
+++ index.php
@@ -127,7 +127,7 @@
 			<?php 
 			    for($i=0;$i<count($res2);$i++){
                     $row=$res2[$i];                    
-                    echo "<a href='readPost.php?id={$row['id']}'>{$row['post_title']}";
+                    echo "<br><a href='readPost.php?id={$row['id']}'>{$row['post_title']}<br>";
                 }
                   
             ?>
