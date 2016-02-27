5135f9261e4bd43d089abef6d0c9d1e45dfd997f
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-27 17:37:46 +0800

    bug

--- config/lang/blogList.css.php
+++ config/lang/blogList.css.php
@@ -1,16 +1,20 @@
 <?php 
     require_once "lang.class.php";
     
-    
-
-        
-        function div5(){
+     function div5(){
             $lan = new Lang();
             $title="";
             if($lan->getLang()=="zh"){
                 $title="推荐帖子";
+                
+                
+                
             }else if($lan->getLang()=="en"){
                 $title="Recommend";
+                
+                
+                
+                
             }
             return $title;
         }
