fc53d75655c2119a248409d0b3e4558bf98958f3
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-27 22:19:42 +0800

    ln -s content/template/ ./

--- content/themes/default/index.php
+++ content/themes/default/index.php
@@ -1,5 +1,5 @@
 <?php 
-
+    echo "<link rel='stylesheet' href='".dirname(__FILE__)."/content/template/css/base.css' type='text/css'  />";
     require_once dirname(__FILE__)."/../../template/articleList.php";
 
 ?>
