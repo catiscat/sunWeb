a201bb55742f2db0e164771a0097398f657473e9
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-27 22:11:43 +0800

    test index.php for require_once

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 <?php 
 	
 	//require_once "temp/temp.php";
-	require_once dirname(__FILE__)."/template/articleList.php";
+	require_once dirname(__FILE__)."/content/template/articleList.php";
 	
 ?>
