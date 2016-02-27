25e743de8309c07cf4270634f6da9f6f4bd93835
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-27 22:28:17 +0800

    ln -s content/template/ ./

--- content/themes/default/index.php
+++ content/themes/default/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link rel="shortcut icon" href="images/duck.ico" />
-    <?php echo "<link rel='stylesheet' href='".dirname(__FILE__)."/css/base.css' type='text/css'  />";  ?>
+    <link rel='stylesheet' href='css/base.css' type='text/css'  />
 
     <title>DuckBlog</title>
   </head>
