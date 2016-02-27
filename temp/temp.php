4344b500ab784ec48873b4f63fb58df779f71866
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-27 21:19:21 +0800

    test index.php for require_once

--- temp/temp.php
+++ temp/temp.php
@@ -1,4 +1,5 @@
 <img src="bigDuck.png"/>
+<img src="images/duck.ico" />
     <link rel="shortcut icon" href="images/duck.ico" >
     <link rel="stylesheet" href="css/base.css" type="text/css"  >
 <?php  
