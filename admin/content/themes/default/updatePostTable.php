64c580d85eb9df27e5de218d7b410b44df9eb182
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-28 20:23:59 +0800

    bug updatePost.php

--- admin/content/themes/default/updatePostTable.php
+++ admin/content/themes/default/updatePostTable.php
@@ -6,9 +6,9 @@
     <form action='postProcess.php' method='post'>
         <table>
             <tr><td>ID</td><td><input readonly='readonly' type='text' name='id' value="$arr[0]['id']" /></td></tr>        
-            <tr><td>post_author</td><td><input type='text' name='post_author' value='<?php echo $arr[0]['post_author'] ?>' /></td></tr>
+            <tr><td>post_author</td><td><input type='text' name='post_author' value=" $arr[0]['post_author'] " /></td></tr>
             <tr><td>post_date</td><td><input type='text' onclick='showTime(this)' name='post_date' value=" $arr[0]['post_date']" /></td></tr>
-		    <tr><td>post_title</td><td><input type='text' name='post_title' value=" echo $arr[0]['post_title'] " /></td></tr>
+		    <tr><td>post_title</td><td><input type='text' name='post_title' value="$arr[0]['post_title'] " /></td></tr>
             <tr><td>post_type</td><td><input type='text' name='post_type' value="$arr[0]['post_type'] " /></td></tr>
             <tr><td>post_content</td><td><textarea type='text' rows='30' cols='50' name='post_content' value=" $arr[0]['post_content']" ></textarea></td></tr>         
 
