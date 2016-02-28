e6eca134e9c88633ff269d1e8a8b1f02fd9ea774
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-28 21:41:04 +0800

    bug addContentTable.php

--- admin/manager/delPost.php
+++ admin/manager/delPost.php
@@ -1,5 +1,6 @@
 <?php 
 	echo "<meta charset='utf-8' />";
 	require_once dirname(__FILE__)."/../../includes/postProcess.php";
-        echo $_SERVER['HTTP_REFERER']."aaaaaaaaaaaaaaaaaaaaaaaaa";
+        
+	header($_SERVER['HTTP_REFERER']);
 ?>
