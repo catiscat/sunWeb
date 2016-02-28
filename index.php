5aee0ea9a62280199dc4ef8d7eac4046ae4d5827
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-28 12:57:24 +0800

    add index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 
 <?php 		
-	require_once "content/template/articleList.php";
-	//如果是当前文件夹，就"content/template/articleList.php"; 或者等价于  "./content/template/articleList.php";  
+	echo "I love scala so much!"
 	
 ?>
