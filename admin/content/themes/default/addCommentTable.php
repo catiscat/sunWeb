18d1a5bc27137af6f68bd40a1d871b2996d8b32f
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-29 15:18:01 +0800

    addComment.php comment_time

--- admin/content/themes/default/addCommentTable.php
+++ admin/content/themes/default/addCommentTable.php
@@ -6,6 +6,7 @@
         <form action='../../../../includes/postProcess.php' method='post'>
             <table>
                 <tr><td>您的大名</td><td><input wrap='physical' type='text' name='comment_author' /></td></tr>
+                <tr><td>评论时间</td><td><input wrap='physical' type='readonly' name='comment_date' value=time() /></td></tr>
                 <tr><td>评论内容</td><td><textarea wrap='physical' rows='10' cols='25' type='text' name='comment_content' ></textarea></td></tr>
                 <input type='hidden' name='flag' value='addcomment' />
 		<input type='hidden' name='tableName' value='comments' />
