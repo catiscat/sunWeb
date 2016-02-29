d258faf4c555337217551f6b6896c0f1104b46a9
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-29 13:28:04 +0800

    add addComment.php

--- admin/content/themes/default/addCommentTable.php
+++ admin/content/themes/default/addCommentTable.php
@@ -5,10 +5,9 @@
             <table>
                 <tr><td>您的大名</td><td><input wrap='physical' type='text' name='comment_author' /></td></tr>
                 <tr><td>评论内容</td><td><textarea wrap='physical' rows='10' cols='25' type='text' name='comment_content' ></textarea></td></tr>
-               <?php echo '<input type='hidden' name='comment_post_id' value=$id />'; ?>
                 <input type='hidden' name='flag' value='addcomment' />
                 <tr><td>验证码</td><td><input type='text' name='checkcode'></td><td><img src='checkCode.php' onclick='this.src='checkCode.php?aa='+Math.random()' /></td></tr>
-                <tr><td><input type='submit' value='$title' /></td><td><input type='reset' value='重新填写' /></td></tr>
+                <tr><td><input type='submit' value=$title/></td><td><input type='reset' value='重新填写' /></td></tr>
                 
             </table><br><br><hr>
        </form>
