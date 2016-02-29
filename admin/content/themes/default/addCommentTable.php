e9995675716617a1525ca74d73017ef351b642a8
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-29 15:35:44 +0800

    addComment.php comment_time bug

--- admin/content/themes/default/addCommentTable.php
+++ admin/content/themes/default/addCommentTable.php
@@ -8,7 +8,7 @@
         <form action='../../../../includes/postProcess.php' method='post'>
             <table>
                 <tr><td>您的大名</td><td><input wrap='physical' type='text' name='comment_author' /></td></tr>
-                <tr><td>评论时间</td><td><input wrap='physical' type='text' name='comment_date' readonly='readonly' value=".date('H:i:sa')."/></td></tr>
+                <tr><td>评论时间</td><td><input wrap='physical' type='text' name='comment_date' readonly='readonly' value=".date('Y-m-d H:i:sa')."/></td></tr>
                 <tr><td>评论内容</td><td><textarea wrap='physical' rows='10' cols='25' type='text' name='comment_content' ></textarea></td></tr>
                 <input type='hidden' name='flag' value='addcomment' />
 		<input type='hidden' name='tableName' value='comments' />
