1a795e2c0b3ba8f52de67c68f184a698a078e4a3
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-29 13:25:53 +0800

    add addComment.php

---
+++ admin/manager/addComment.php
@@ -0,0 +1,5 @@
+<?php 
+    require_once (dirname(__FILE__)."/../content/themes/default/addCommentTable.php");
+    addCommentTable("添加博文");
+
+?>
--- content/themes/default/readPost.php
+++ content/themes/default/readPost.php
@@ -71,18 +71,9 @@
 		    <!--显示添加评论-->
 		    
 		    <h3>添加评论</h3>
-                <form action="userCommentProcess.php" method="post">
-                    <table>
-                        <tr><td>您的大名</td><td><input wrap="physical" type="text" name="comment_author" /></td></tr>
-                        <tr><td>评论内容</td><td><textarea wrap="physical" rows="10" cols="25" type="text" name="comment_content" ></textarea></td></tr>
-                       <?php echo "<input type='hidden' name='comment_post_id' value=$id />"; ?>
-                        <input type="hidden" name="flag" value="addcomment" />
-                        <tr><td>验证码</td><td><input type="text" name="checkcode"></td><td><img src="checkCode.php" onclick="this.src='checkCode.php?aa='+Math.random()" /></td></tr>
-                        <tr><td><input type="submit" value="添加评论" /></td><td><input type="reset" value="重新填写" /></td></tr>
-                        
-                    </table><br><br><hr>
-                </form>
-                
+		    <?php
+		    require_once dirname(__FILE__)."/../../../admin/manager/addComment.php";
+            ?>    
             <!--显示所有关于该文章的评论-->
             <br></br><h3>所有评论</h3><br><hr>
             <?php 
