6809e9bfa36a3627f3d94a22e1553a716df246d8
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-29 13:43:48 +0800

    add checkCode.php bug

--- admin/content/themes/default/addCommentTable.php
+++ admin/content/themes/default/addCommentTable.php
@@ -6,7 +6,7 @@
                 <tr><td>您的大名</td><td><input wrap='physical' type='text' name='comment_author' /></td></tr>
                 <tr><td>评论内容</td><td><textarea wrap='physical' rows='10' cols='25' type='text' name='comment_content' ></textarea></td></tr>
                 <input type='hidden' name='flag' value='addcomment' />
-                <tr><td>验证码</td><td><input type='text' name='checkcode'></td><td><img src='../../../includes/checkCode.php' onclick="this.src='../../../includes/checkCode.php?aa='+Math.random()" /></td></tr>
+                <tr><td>验证码</td><td><input type='text' name='checkcode'></td><td><img src='../../../includes/checkCode.php' onclick=this.src='../../../includes/checkCode.php?aa='+Math.random() /></td></tr>
                 <tr><td><input type='submit' value=$title/></td><td><input type='reset' value='重新填写' /></td></tr>
                 
             </table><br><br><hr>
