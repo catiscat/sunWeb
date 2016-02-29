42068b0cf9c352ec37d3395e6d141ca38df8b078
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-29 19:03:34 +0800

    updateComment.php bug

--- admin/manager/updateComment.php
+++ admin/manager/updateComment.php
@@ -13,12 +13,12 @@
     require_once dirname(__FILE__)."/../../includes/PostService.class.php";
     $sqlHelper=new SqlHelper();
 
-    $id=mysql_real_escape_string(strip_tags($_GET['id']));
+    $idd=mysql_real_escape_string(strip_tags($_GET['id']));
     //通过id得到该评论的其他信息
     //查询数据库，调用sqlHelper
     
     $postService=new PostService("comments");
-    $arr=$postService->getPostById($id);
+    $arr=$postService->getPostById($idd);
     
     ?>
     
