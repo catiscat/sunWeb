4039d0f726b75b856658c79a7a2415e466bfd185
catiscat <JohnWillimsunnyday@tutanota.com>
2016-02-29 15:57:00 +0800

    updateCommentTable.php

---
+++ admin/content/themes/default/updateCommentTable.php
@@ -0,0 +1,18 @@
+<?php 
+	
+	function updatePostTable($arr){
+	
+	    echo "
+    <form action='../../../../includes/postProcess.php' method='post'>
+        <table>       
+            <tr><td>post_author</td><td><input type='text' name='post_author' value=" .$arr[0]['comment_author']." /></td></tr>
+            <tr><td>post_date</td><td><input type='text' name='post_date' value=" .$arr[0]['comment_date']." /></td></tr>
+            <tr><td>post_content</td><td><textarea type='text' rows='30' cols='50' name='post_content' value=" .$arr[0]['comment_content']." ></textarea></td></tr>         
+            <input type='hidden' name='flag' value='updatecomment' />
+            <input type='hidden' name='tableName' value='comments' />
+            <tr><td><input type='submit' value='修改' /></td><td><input type='reset' value='重新填写' /></td></tr>
+        </table>
+    </form>
+            ";
+    }
+?>
